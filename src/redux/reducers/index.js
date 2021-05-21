ce2ee8a8cfdb01cb53670dd2522302351fe9eb1d
Huy <huytdps13400@fpt.edu.vn>
2021-05-21 16:42:12 +0700

    reducers

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,8 +1,9 @@
 import {combineReducers} from 'redux';
 import {TokenReducer} from './TokenReducer';
-
+import {NewsReducer} from './NewsReducer';
 const rootReducer = combineReducers({
   ...TokenReducer,
+  ...NewsReducer,
 });
 
 export default rootReducer;
